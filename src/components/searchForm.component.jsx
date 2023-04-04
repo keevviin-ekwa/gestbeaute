44768dc3e5bf8fb0d6c861f9bfdfed06c213e7d9
KEVIN EKWA NDE <EKWA@GROUPEDK.LOCAL>
2023-04-04 18:32:19 +0100

    Reservation input continue

--- src/components/searchForm.component.jsx
+++ src/components/searchForm.component.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 export default function SearchForm() {
   return (
-    <div className='search_form'>
-        
-           <div>
-             <input className='email_input form-control' placeholder='Email' type="email"/>
-           </div>
-           <div>
-           <input className='email_input form-control'  type="date"/>
-           </div>
-           <div>
-            <button className='btn btn-primary'>Continuer</button>
-           
-        </div>
+    <div>
+      <div className='search_form'>
+          <div className="row">
+            <div className="col title">
+              <span className='title_underline'>Reservation</span>
+            </div>
+          </div>
+
+          <div className=" d-flex justify-content-sm-evenly align-items-center my-4">
+             <input placeholder='Email' type="text" className='form-control email_input' />
+             <input type="Date" className='form-control date_input' />
+             <button className='search_button'>Continuer</button>
+          </div>
+      </div>
     </div>
   )
 }
